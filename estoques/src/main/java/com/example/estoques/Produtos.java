b8481681fc5ddb1c82692b7dc49431befa3ef5db
TiagoCSoares <tiagocsoares22@gmail.com>
2024-04-07 22:03:40 -0300

    Adding the class Produtos

---
+++ estoques/src/main/java/com/example/estoques/Produtos.java
@@ -0,0 +1,61 @@
+package com.example.estoques;
+
+import java.math.BigDecimal;
+
+public class Produtos {
+
+    private Integer id;
+    private String nome;
+    private Integer quantidade;
+    private String categoria;
+    private BigDecimal preco;
+
+
+    public Produtos(Integer id, String nome, Integer quantidade, String categoria, BigDecimal preco) {
+        this.id = id;
+        this.nome = nome;
+        this.quantidade = quantidade;
+        this.categoria = categoria;
+        this.preco = preco;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+}
---
+++ estoques/src/main/java/com/example/estoques/arquivos/LerArquivo.java
@@ -0,0 +1,4 @@
+package com.example.estoques.arquivos;
+
+public class LerArquivo {
+}
